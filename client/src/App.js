20776039b07056e3fed4bb6ba4ba973faa86d7cf
cox9176 <cox9176@bears.unco.edu>
2023-01-15 12:00:57 -0700

    Add Login Form
    Login form currently validates against a const variable with no token authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,49 @@
 import React, { useEffect, useState } from 'react'
+import LoginForm from './components/LoginForm'
 
 function App() {
+
+  // temp user details to validate against for form
+  // once auth is built then can just confirm from auth
+  const testUser = {
+    email: "test@test.test",
+    password: "testPassword"
+  }
+
+  // const to store user details and error message
+  // needed to determine if user is logged in when first accessing site
+  // token is currently unused
+  const [user, setUser] = useState({name: "", email: "", token: ""});
+  const [error, setError] = useState("");
+
+
+  // user details held for login verification
+  // *** ONCE AUTH IS BUILT THIS NEEDS TO CHANGE TO VALIDATE WITH TOKEN
+  const Login = details => {
+    console.log(details)
+
+    if (details.email == testUser.email && details.password == testUser.password) {
+      console.log("Logged in");
+      setUser({
+        name: details.name,
+        email: details.email
+      });
+      setError("")
+    } else {
+      console.log("Details do not match")
+      setError("Details do not match")
+    }
+  }
+
+  // reset user details when logged out
+  const Logout = () => {
+    setUser({
+      name: "",
+      email: ""
+    })
+  }
+
+  // backendData stuff?
 
   const [backendData, setBackendData] = useState([{}])
 
@@ -15,6 +58,10 @@ function App() {
     )
   }, [])
 
+  // return loading backendData
+
+  // also return login form or temporary logged in page
+
   return (
     <div>
       {(typeof backendData.users === 'undefined') ? (
@@ -23,6 +70,16 @@ function App() {
         backendData.users.map((user, i) => (
           <p key={i}>{i + 1}. {user}</p>
         ))
+      )},
+      {(user.email != "") ? (
+
+        // temp logged in page or login form
+        <div className="welcome">
+          <h2>Welcome, <span>{user.name}</span></h2>
+          <button onClick={Logout}>Logout</button>
+        </div>
+      ) : (
+        <LoginForm Login={Login} error={error}/>
       )}
     </div>
   )
