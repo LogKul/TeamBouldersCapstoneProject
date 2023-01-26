7af4a7dcd0651eff128106c048a1a61e05426a64
prilika1 <53357004+prilika1@users.noreply.github.com>
2023-01-26 15:07:49 -0700

    Current working draft of associations

--- api/models/index.js
+++ api/models/index.js
@@ -1,6 +1,6 @@
 const config = require("../config/db.config.js");
 
-const Sequelize = require("sequelize");
+const { Sequelize, DataTypes } = require("sequelize");
 const sequelize = new Sequelize(
     config.DB,
     config.USER,
@@ -37,6 +37,23 @@ db.user = require("../models/user.model.js")(sequelize, Sequelize);
 db.game = require("../models/game.model.js")(sequelize, Sequelize);
 db.chat = require("../models/chat.model.js")(sequelize, Sequelize);
 db.report = require("../models/report.model.js")(sequelize, Sequelize);
+
+/* Associations */
+(db.game).hasOne(db.user, { as: "player1", foreignKey: "player1" });
+(db.game).hasOne(db.user, { as: "player2", foreignKey: "player2" });
+//(db.users).hasMany(db.game, { as: "player1", foreignKey: "player1" });
+//(db.users).hasMany(db.game, { as: "player2", foreignKey: "player2" });
+(db.users).belongsTo(db.game);
+
+(db.report).hasOne(db.user, { as: "reported_user", foreignKey: "reported_user" });
+(db.user).belongsTo(db.report);
+
+(db.chat).hasOne(db.game, { as: "game_id", foreignKey: "game_id" });
+(db.chat).hasOne(db.user, { as: "player", foreignKey: "player" });
+(db.user).belongsTo(db.chat);
+(db.game).belongsTo(db.chat);
+
+
 
 /*
 db.role.belongsToMany(db.user, {
