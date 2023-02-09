ad312c87c1bbce969e1787bf9860601d0565d5fb
cox9176 <89417732+cox9176@users.noreply.github.com>
2023-02-08 18:28:16 -0700

    Checkers Piece Movement Constraints

--- client/src/components/checkers/Checkers.jsx
+++ client/src/components/checkers/Checkers.jsx
@@ -1,6 +1,6 @@
+import { useRef } from "react"
 import Tile from "./tile/Tile"
 import "./Checkers.css"
-import ReactDOM from "react-dom"
 
 const vertAxis = [1, 2, 3, 4, 5, 6, 7, 8]
 const horzAxis = [1, 2, 3, 4, 5, 6, 7, 8]
@@ -24,49 +24,70 @@ for (let i = 0; i < 8; i++) {
     }
 }
 
-let activePiece = undefined
-
-const grabPiece = (e) => {
-    e.preventDefault()
-
-    const element = e.target
-
-    if (element.classList.contains("checkers-piece")) {
-
-        const x = e.clientX - 25
-        const y = e.clientY - 25
-
-        element.style.position = "absolute"
-        element.style.left = x.toString()+'px'
-        element.style.top = y.toString()+'px'
-
-        activePiece = element
-    }
-}
-
-const movePiece = (e) => {
-    e.preventDefault()
-
-    if (activePiece) {
-
-        const x = e.clientX - 25
-        const y = e.clientY - 25
-
-        activePiece.style.position = "absolute"
-        activePiece.style.left = x.toString()+'px'
-        activePiece.style.top = y.toString()+'px'
-    }
-}
-
-const dropPiece = (e) => {
-    e.preventDefault()
-
-    if (activePiece) {
-        activePiece = undefined
-    }
-}
-
 export default function Checkers() {
+    const checkersBoardRef = useRef(null)
+
+    let activePiece = undefined
+
+    const grabPiece = (e) => {
+        e.preventDefault()
+
+        const element = e.target
+
+        if (element.classList.contains("checkers-piece")) {
+
+            const x = e.clientX - 25
+            const y = e.clientY - 25
+
+            element.style.position = "absolute"
+            element.style.left = x.toString()+'px'
+            element.style.top = y.toString()+'px'
+
+            activePiece = element
+        }
+    }
+
+    const movePiece = (e) => {
+        e.preventDefault()
+
+        const checkersBoard = checkersBoardRef.current
+
+        if (activePiece && checkersBoard) {
+            const minX = checkersBoard.offsetLeft
+            const minY = checkersBoard.offsetTop
+            const maxX = checkersBoard.offsetLeft + checkersBoard.clientWidth - 50
+            const maxY = checkersBoard.offsetTop + checkersBoard.clientHeight - 50
+            const x = e.clientX - 25
+            const y = e.clientY - 25
+
+            activePiece.style.position = "absolute"
+
+            if (x < minX) {
+                activePiece.style.left = minX.toString()+"px"
+            } else if (x > maxX) {
+                activePiece.style.left = maxX.toString()+"px"
+            } else {
+                activePiece.style.left = x.toString()+"px"
+            }
+
+            if (y < minY) {
+                activePiece.style.top = minY.toString()+"px"
+            } else if (y > maxY) {
+                activePiece.style.top = maxY.toString()+"px"
+            } else {
+                activePiece.style.top = y.toString()+"px"
+            }
+        }
+    }
+
+    const dropPiece = (e) => {
+        e.preventDefault()
+
+        if (activePiece) {
+            activePiece = undefined
+        }
+    }
+
     let board = []
 
     for (let i = 0; i < horzAxis.length; i++) {
@@ -90,7 +111,8 @@ export default function Checkers() {
                 onMouseMove={e => movePiece(e)} 
                 onMouseDown={e => grabPiece(e)} 
                 onMouseUp={e => dropPiece(e)}
-                id="board">
+                id="board"
+                ref={checkersBoardRef}>
                     
                 {board}
             </div>
