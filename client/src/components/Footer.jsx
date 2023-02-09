c2d594022029aa1dbe3f09e1d4fc81a3659d9b7d
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2023-02-08 18:44:06 -0700

    add footer content

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Footer() {
+    return(
+        <div className='footer'>
+            <div className='footer-left'>
+                <p className='subtitle'>Left footer text.</p>
+            </div>
+            <div className='footer-center'>
+                <p className='subtitle'>Center footer text.</p>
+            </div>
+            <div className='footer-right'>
+                <p className='subtitle'>Right footer text.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
