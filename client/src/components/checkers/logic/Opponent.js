c204c90132a816c624a84a9dfae15180f6fdd8ef
cox9176 <89417732+cox9176@users.noreply.github.com>
2023-02-23 15:03:51 -0700

    AI Takes Multiple Pieces

--- client/src/components/checkers/logic/Opponent.js
+++ client/src/components/checkers/logic/Opponent.js
@@ -1,4 +1,5 @@
 import AILogic from "./AILogic"
+import Logic from "./Logic"
 
 export default class Opponent {
     generateResponse(gameMode, difficulty, boardState, oppColor) {
@@ -20,53 +21,91 @@ export default class Opponent {
     }
 
     aiEasyMove(boardState, oppColor) {
-        const aiLogic = new AILogic()
+        let pieceMove = undefined
+        let checkMultiMove = true
+        let newBoardState = undefined
+        while (checkMultiMove) {
+            pieceMove = this.getRandomMove(this.getMovablePieces(boardState, oppColor))
+            newBoardState = this.movePiece(boardState, pieceMove, oppColor)
+            checkMultiMove = false
+        }
+        return newBoardState
+    }
 
-        const piecesMoves = []
+    aiMediumMove(boardState) {
 
+        console.log("choosing medium move")
+        return boardState
+    }
+
+    aiHardMove(boardState) {
+
+        console.log("choosing hard move")
+        return boardState
+    }
+
+    getMovablePieces(boardState, oppColor) {
+        const pieces = []
         boardState?.forEach(p => {
-                    if (p.color === oppColor) {
-                        const move = aiLogic.findPossibleMove(p.x, p.y, p.color, p.king, boardState)
-                        if (move !== undefined) {
-                            piecesMoves.push(move)
-                        }
-                    }
-                })
+            if (p.color === oppColor) {
+                const move = this.getPieceMoves(p.x, p.y, boardState, oppColor, p.king, false)
+                if (move !== undefined) {
+                    pieces.push(move)
+                }
+            }
+        })
+        return pieces
+    }
 
-        const randPiece = Math.floor(Math.random() * piecesMoves.length)
-        const randMove = Math.floor(Math.random() * piecesMoves[randPiece][2].length)
+    getPieceMoves(px, py, boardState, oppColor, king, continuedAttack) {
+        const aiLogic = new AILogic()
+        return aiLogic.findPossibleMove(px, py, oppColor, king, boardState, continuedAttack)
+    }
 
-        const x = piecesMoves[randPiece][2][randMove][0]
-        const y = piecesMoves[randPiece][2][randMove][1]
+    getRandomMove(movablePieces) {
+        const randPiece = Math.floor(Math.random() * movablePieces.length)
+        const randMove = Math.floor(Math.random() * movablePieces[randPiece][2].length)
+        return [movablePieces[randPiece][0], movablePieces[randPiece][1], movablePieces[randPiece][2][randMove]]
+    }
 
-        var removeX = 0
-        var removeY = 0
-        var spliceVal = 0
+    movePiece(boardState, pieceMove, oppColor) {
+        const logic = new Logic()
 
-        console.log(x)
-        console.log(y)
+        const x = pieceMove[2][0]
+        const y = pieceMove[2][1]
 
-        const newBoardState = boardState.map((p) => {
-            if (p.x === piecesMoves[randPiece][0] && p.y === piecesMoves[randPiece][1]) {
+        let removeX = 0
+        let removeY = 0
+        let spliceVal = 0
+
+        let newBoardState = undefined
+        let newPiece = undefined
+
+        newBoardState = boardState.map((p) => {
+            if (p.x === pieceMove[0] && p.y === pieceMove[1]) {
                 if (p.x + 2 === x) {
                     if (p.y + 2 === y) {
                         removeX = p.x + 1
                         removeY = p.y + 1
                         spliceVal = 1
+                        console.log("ai took x: "+removeX+" y: "+removeY)
                     } else {
                         removeX = p.x + 1
                         removeY = p.y - 1
                         spliceVal = 1
+                        console.log("ai took x: "+removeX+" y: "+removeY)
                     }
                 } else if (p.x - 2 === x) {
                     if (p.y + 2 === y) {
                         removeX = p.x - 1
                         removeY = p.y + 1
                         spliceVal = 1
+                        console.log("ai took x: "+removeX+" y: "+removeY)
                     } else {
                         removeX = p.x - 1
                         removeY = p.y - 1
                         spliceVal = 1
+                        console.log("ai took x: "+removeX+" y: "+removeY)
                     }
                 }
                 if (p.color === 0) {
@@ -83,24 +122,18 @@ export default class Opponent {
                 p.x = x
                 p.y = y
             }
+            newPiece = p
             return p
         })
         const index = newBoardState.indexOf(newBoardState.find((p) => p.x === removeX && p.y === removeY))
         newBoardState.splice(index, spliceVal)
+        if (pieceMove[0] === x - 2 || pieceMove[0] === x + 2) {
+            if (logic.additionalMoveExists(x, y, pieceMove[0], pieceMove[1], oppColor, pieceMove[3], newBoardState)) {
+                let newPieceMove = this.getPieceMoves(x, y, newBoardState, oppColor, newPiece[3], true)
+                newPieceMove = [newPieceMove[0], newPieceMove[1], newPieceMove[2][0], newPieceMove[3]]
+                newBoardState = this.movePiece(newBoardState, newPieceMove, oppColor)
+            }
+        }
         return newBoardState
-    }
-
-    aiMediumMove(boardState) {
-        const aiLogic = new AILogic()
-
-        console.log("choosing medium move")
-        return boardState
-    }
-
-    aiHardMove(boardState) {
-        const aiLogic = new AILogic()
-
-        console.log("choosing hard move")
-        return boardState
     }
 }
