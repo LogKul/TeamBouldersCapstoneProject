2e42fbb83508844ab3deb51b1afbdca4ac9f4692
cox9176 <89417732+cox9176@users.noreply.github.com>
2023-03-03 14:34:12 -0700

    Fix eslint errors in Leaderboard.jsx

--- client/src/components/pages/Leaderboard.jsx
+++ client/src/components/pages/Leaderboard.jsx
@@ -13,7 +13,7 @@ const Leaderboard = () => {
                 <br></br>
                 <h2>This page will include links to:</h2>
                 <ul>
-                    <Link to="/account"><li>Account for viewing a specific player's account</li></Link>
+                    <Link to="/account"><li>Account for viewing a specific player&apos;s account</li></Link>
                 </ul>
             </div>
             <Footer />
