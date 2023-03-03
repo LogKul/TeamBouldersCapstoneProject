4818267cce3a062067cfcf883b2789a342cd3145
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2023-03-02 18:46:14 -0700

    fix missing react import

--- client/src/components/pages/Matchmaking.jsx
+++ client/src/components/pages/Matchmaking.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { React, useState } from "react"
 import Header from '../Header'
 import Footer from '../Footer'
 import Checkers from "../checkers/Checkers"
