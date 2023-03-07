f1963e7248c82f95ad6c4a7c0d2c7a953f45f9ed
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2023-03-07 15:41:46 -0700

    add demo implementation of modal to home page

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../Header'
 import Footer from '../Footer'
+import Modal from '../Modal'
 
 const Home = () => {
+
+    const [modalIsOpen, setModalIsOpen] = useState(false)
+
+    function openModal() {
+        setModalIsOpen(true);
+    }
+
+    function closeModal() {
+        setModalIsOpen(false);
+    }
+
     return (
         <div>
             <Header />
             <div className='content-wrap'>
                 <p>This will be the homepage!</p>
                 <a href="/start"><button>Play Game</button></a>
+                <br />
+                <button onClick={openModal}>Open Pop-Up</button>
+                <Modal isOpen={modalIsOpen} closeModal={closeModal}>
+                    <h1>This is a pop-up!</h1>
+                    <p>Pretty neat huh?</p>
+                </Modal>
             </div>
             <Footer />
         </div>
