42ed851b331def8b75c832ffcfc234ce89d23f6f
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2023-03-30 16:20:36 -0600

    first draft of new play page ui

--- client/src/components/pages/Play.jsx
+++ client/src/components/pages/Play.jsx
@@ -1,31 +1,32 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import Header from '../Header'
 import Footer from '../Footer'
+import '../../styles/play.scss'
 
 const Play = () => {
     return (
         <div>
             <Header />
             <div className='content-wrap'>
-                <h2>P1 V.S. Computer</h2>
+                {/* <h2>P1 V.S. Computer</h2>
                 <ul>
                     <li><Link to="/game/0/0/0/0">Easy</Link></li>
                     <li><Link to="/game/0/1/0/0">Medium</Link></li>
                     <li><Link to="/game/0/2/0/0">Hard</Link></li>
-                </ul>
+                </ul> */}
 
-                <br></br>
-                <br></br>
-                <hr></hr>
+                {/* <Link to="/game/1/0/0/0"><h2>P1 V.S. P2 Local</h2></Link> */}
 
-                <Link to="/game/1/0/0/0"><h2>P1 V.S. P2 Local</h2></Link>
-
-                <br></br>
-                <br></br>
-                <hr></hr>
-
-                <Link to="/matchmaking"><h2>P1 V.S. P2 Online</h2></Link>
+                {/* <Link to="/matchmaking"><h2>P1 V.S. P2 Online</h2></Link> */}
+                <h1>Choose Your Opponent</h1>
+                <div className='flex-container'>
+                    <div className='flex-child'>
+                        <a href="/game/0/0/0/0"><button className='large-button'>Versus Computer</button></a>
+                    </div>
+                    <div className='flex-child'>
+                        <a href="/matchmaking"><button className='large-button'>Online Versus</button></a>
+                    </div>
+                </div>
 
             </div>
             <Footer />
