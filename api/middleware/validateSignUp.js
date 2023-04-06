4e87721322c2701324cf79b0efabbeffa2813821
Hudson Turner <30182083+hturner80631@users.noreply.github.com>
2023-04-06 17:24:01 -0600

    Mitigating Roles Length Bug

--- api/middleware/validateSignUp.js
+++ api/middleware/validateSignUp.js
@@ -21,7 +21,7 @@ validateNoDuplicateUsername = (req, res, next) => {
 checkRolesExisted = (req, res, next) => {
     if (req.body.roles) {
         for (let i = 0; i < req.body.roles.length; i++) {
-            if (!ROLES.includes(req.body.roles[i])) {
+            if (!ROLES.includes(req.body.roles[i]) || req.body.roles.length > 3) {
                 res.status(400).send({
                     message: "Failed! Role does not exist = " + req.body.roles[i]
                 });
