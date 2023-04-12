7016b51a34f7bf38bb162241c8d186c6e0fd954a
prilika1 <53357004+prilika1@users.noreply.github.com>
2023-04-12 13:47:42 -0600

    dashboard, pw update fix, account page

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import Header from '../Header'
 import Footer from '../Footer'
 import Modal from '../Modal'
@@ -21,11 +22,22 @@ const Home = () => {
             <Header />
             <div className='content-wrap'>
                 <h1>Dashboard</h1>
-                <div className='flex-container'>
-                    <div className='flex-child'>
-                        <h3>Your MMR</h3>
+                <div className='flex-container-outer-home'>
+                    <div className='flex-container-vertical-home'>
+                        <div className='flex-child-home-25'>
+                            <h3 className='underline'>Your MMR</h3>
+                            <h2>{sessionStorage.getItem("mmr")}</h2>
+                        </div>
+                        <div className='flex-child-home-25'>
+                            <h4>Wins: {sessionStorage.getItem("wins") + "\n"}</h4>
+                            <h4>Losses: {sessionStorage.getItem("losses") + "\n"}</h4>
+                            {sessionStorage.getItem("losses") == 0
+                                ? <h4>W/L: Perfect{"\n"}</h4>
+                                : <h4>W/L: {(sessionStorage.getItem("wins") / sessionStorage.getItem("losses")).toFixed(2) + "\n"}</h4>
+                            }
+                        </div>
                     </div>
-                    <div className='flex-child'>
+                    <div className='flex-child-home-50'>
                         <a href="/play"><button>Play Game</button></a>
                         <br />
                         <button onClick={openModal}>Open Pop-Up</button>
@@ -34,10 +46,13 @@ const Home = () => {
                             <p>Pretty neat huh?</p>
                         </Modal>
                     </div>
-                    <div className='flex-child'>
-                        <h3>Your Wins:     0</h3>
-                        <h3>Your Losses:   0</h3>
-                        <h3>Your W/L:      0</h3>
+                    <div className='flex-container-vertical-home'>
+                        <div className='flex-child-home-25'>
+                            <Link to={"/recordings/" + sessionStorage.getItem("user")}><h3>Game History</h3></Link>
+                        </div>
+                        <div className='flex-child-home-25'>
+                            <Link to={"/account"}><h3>Your Account</h3></Link>
+                        </div>
                     </div>
                 </div>
             </div>
