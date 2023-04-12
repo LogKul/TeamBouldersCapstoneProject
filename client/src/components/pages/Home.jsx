4a7fecc49f27d836d50cc6d58131ec069f2660e8
prilika1 <53357004+prilika1@users.noreply.github.com>
2023-04-12 10:45:54 -0600

    Very interesting and funny broken homepage

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Header from '../Header'
 import Footer from '../Footer'
 import Modal from '../Modal'
+import '../../styles/home.scss'
 
 const Home = () => {
 
@@ -19,14 +20,26 @@ const Home = () => {
         <div>
             <Header />
             <div className='content-wrap'>
-                <p>This will be the homepage!</p>
-                <a href="/play"><button>Play Game</button></a>
-                <br />
-                <button onClick={openModal}>Open Pop-Up</button>
-                <Modal isOpen={modalIsOpen} closeModal={closeModal}>
-                    <h1>This is a pop-up!</h1>
-                    <p>Pretty neat huh?</p>
-                </Modal>
+                <h1>Dashboard</h1>
+                <div className='flex-container'>
+                    <div className='flex-child'>
+                        <h3>Your MMR</h3>
+                    </div>
+                    <div className='flex-child'>
+                        <a href="/play"><button>Play Game</button></a>
+                        <br />
+                        <button onClick={openModal}>Open Pop-Up</button>
+                        <Modal isOpen={modalIsOpen} closeModal={closeModal}>
+                            <h1>This is a pop-up!</h1>
+                            <p>Pretty neat huh?</p>
+                        </Modal>
+                    </div>
+                    <div className='flex-child'>
+                        <h3>Your Wins:     0</h3>
+                        <h3>Your Losses:   0</h3>
+                        <h3>Your W/L:      0</h3>
+                    </div>
+                </div>
             </div>
             <Footer />
         </div>
