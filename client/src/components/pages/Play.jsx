3072c2690d21a2b78bfaf8ecc6b3a1789eaff739
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2023-04-13 14:17:35 -0600

    add icons to buttons

--- client/src/components/pages/Play.jsx
+++ client/src/components/pages/Play.jsx
@@ -2,29 +2,21 @@ import React from 'react'
 import Header from '../Header'
 import Footer from '../Footer'
 import '../../styles/play.scss'
+import { GiCrossedSwords } from "react-icons/gi";
+import { FaRobot } from "react-icons/fa";
 
 const Play = () => {
     return (
         <div>
             <Header />
             <div className='content-wrap'>
-                {/* <h2>P1 V.S. Computer</h2>
-                <ul>
-                    <li><Link to="/game/0/0/0/0">Easy</Link></li>
-                    <li><Link to="/game/0/1/0/0">Medium</Link></li>
-                    <li><Link to="/game/0/2/0/0">Hard</Link></li>
-                </ul> */}
-
-                {/* <Link to="/game/1/0/0/0"><h2>P1 V.S. P2 Local</h2></Link> */}
-
-                {/* <Link to="/matchmaking"><h2>P1 V.S. P2 Online</h2></Link> */}
-                <h1>Choose Your Opponent</h1>
+                <h1 className>Choose Your Opponent</h1>
                 <div className='flex-container'>
                     <div className='flex-child'>
-                        <a href="/game"><button className='large-button'>Versus Computer</button></a>
+                        <a href="/game"><button className='large-button'>Versus <FaRobot/> Computer</button></a>
                     </div>
                     <div className='flex-child'>
-                        <a href="/matchmaking"><button className='large-button'>Online Versus</button></a>
+                        <a href="/matchmaking"><button className='large-button'>PVP <GiCrossedSwords/> Online</button></a>
                     </div>
                 </div>
 
