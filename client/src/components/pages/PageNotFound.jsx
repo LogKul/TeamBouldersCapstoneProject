cb84181398000fff86add27396ec5cde3929ff08
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2023-04-20 20:49:41 -0600

    remove extra br tags and add button back to home and account apges

--- client/src/components/pages/PageNotFound.jsx
+++ client/src/components/pages/PageNotFound.jsx
@@ -8,12 +8,10 @@ function PageNotFound() {
         <div>
             <Header />
             <div className='content-wrap'>
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
                 <h1>404</h1>
                 <h3>Page Not Found.</h3>
+                <a href="home"><button>Home</button></a>
+                <a href="account"><button>Account</button></a>
             </div>
             <Footer />
         </div>
