a4ce1bf736bb6fbd36c4168391c633fc5b57a5c2
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2023-04-22 16:12:27 -0600

    add more content to the landing page

--- client/src/components/pages/Landing.jsx
+++ client/src/components/pages/Landing.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import Header from '../Header'
 import Footer from '../Footer'
+import '../../styles/landing.scss'
+import LeaderboardDisplay from '../LeaderboardDisplay'
+import { FaRobot } from "react-icons/fa";
+import { GiCrossedSwords, GiRank3 } from "react-icons/gi";
+import { ImStatsDots } from "react-icons/im"
 
 function Landing() {
 
@@ -9,7 +14,23 @@ function Landing() {
             <Header />
             <div className='content-wrap'>
                 <h1>Welcome To Checkers!</h1>
-                <a href="/Login"><button>Login</button></a>
+                <p>Please sign to access all functions on the site.</p>
+                <a href="/Login"><button className='large-button'>Login</button></a>
+                <div className='flex-container'>
+                    <div className='flex-child'>
+                        <h4>Features:</h4>
+                        <ul className='landing-page-list'>
+                            <li><FaRobot/> Play Vs. AI</li>
+                            <li><GiCrossedSwords/> Play online via matchmaking.</li>
+                            <li><GiRank3/> Track your account stats and matchmaking rank.</li>
+                            <li><ImStatsDots/> Climb the leaderboard.</li>
+                        </ul>
+                    </div>
+                    <div className='flex-child'>
+                        <h4>Current Leaderboard:</h4>
+                        <LeaderboardDisplay />
+                    </div>
+                </div>
             </div>
             <Footer />
         </div>
