5fc3dd8fe2466a45f188a43f23a75063c2e92e4b
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2023-04-22 16:17:13 -0600

    update outdated page with new ui

--- client/src/components/pages/Outdated.jsx
+++ client/src/components/pages/Outdated.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import Header from '../Header'
+import Footer from '../Footer'
+
 function Outdated() {
 
     return (
         <div>
-            <p>This browser Not Supported!</p>
-            <p>You are currently using a browser or device that is not supported by our application.</p>
-            <p>Please download a supported browser or us a different device in order to use our application!</p>
-            <a href="https://www.mozilla.org/en-US/firefox/new/">Mozilla Firefox Browser for Desktop</a>
-            <a href="https://www.google.com/chrome/">Google Chrome Browser for Desktop</a>
+            <Header />
+            <div className='content-wrap'>
+                <h1>This Browser Is Not Supported!</h1>
+                <p>You are currently using a browser or device that is not supported by our application.</p>
+                <p>Please download a supported browser or us a different device in order to use our application.</p>
+                <a href="https://www.mozilla.org/en-US/firefox/new/"><button>Mozilla Firefox Browser</button></a>
+                <a href="https://www.google.com/chrome/"><button>Google Chrome Browser</button></a>
+            </div>
+            <Footer />
         </div>
     )
 }
