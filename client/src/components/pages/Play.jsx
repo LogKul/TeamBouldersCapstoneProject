175b44b6034433340f4fd8f106fddaf026c3442f
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2023-04-24 12:10:09 -0600

    add more info regarding modes to the play page

--- client/src/components/pages/Play.jsx
+++ client/src/components/pages/Play.jsx
@@ -12,10 +12,22 @@ const Play = () => {
                 <h1>Choose Your Opponent</h1>
                 <div className='flex-container'>
                     <div className='flex-child'>
-                        <a href="/game"><button className='large-button'>Versus <FaRobot/> Computer</button></a>
+                        <a href="/game"><button className='large-button'><FaRobot/> Offline Against AI <FaRobot/></button></a>
+                        <ul className='unordered-list'>
+                            <li>No time limit.</li>
+                            <li>Stats are not saved.</li>
+                            <li>No penalty for leaving game.</li>
+                        </ul>
                     </div>
                     <div className='flex-child'>
                         <a href="/matchmaking"><button className='large-button'>PVP <GiCrossedSwords/> Online</button></a>
+                        <ul className='unordered-list'>
+                            <li>Automatic matchmaking</li>
+                            <li>Timed games.</li>
+                            <li>Stats are saved.</li>
+                            <li>Taking too long to play forfeits.</li>
+                            <li>Leaving the game forfeits.</li>
+                        </ul>
                     </div>
                 </div>
 
