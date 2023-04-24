5a4275d757dae9a162fa2db32b221395ee93bef0
Hudson Turner <30182083+hturner80631@users.noreply.github.com>
2023-04-23 23:33:27 -0600

    Updating Outdated

--- client/src/components/pages/Outdated.jsx
+++ client/src/components/pages/Outdated.jsx
@@ -8,19 +8,13 @@ function Outdated() {
 
     return (
         <div>
+            
+            <Header />
             <h1><FaRobot/><br></br>This Browser Is Not Supported!</h1>
             <p>You are currently using a browser or device that is not supported by our application.</p>
             <p>Please download a supported browser or us a different device in order to use our application.</p>
             <a href="https://www.mozilla.org/en-US/firefox/new/"><button>Mozilla Firefox Browser</button></a>
             <a href="https://www.google.com/chrome/"><button>Google Chrome Browser</button></a>
-            <Header />
-            <div className='content-wrap'>
-                <h1>This Browser Is Not Supported!</h1>
-                <p>You are currently using a browser or device that is not supported by our application.</p>
-                <p>Please download a supported browser or us a different device in order to use our application.</p>
-                <a href="https://www.mozilla.org/en-US/firefox/new/"><button>Mozilla Firefox Browser</button></a>
-                <a href="https://www.google.com/chrome/"><button>Google Chrome Browser</button></a>
-            </div>
             <Footer />
         </div>
     )
