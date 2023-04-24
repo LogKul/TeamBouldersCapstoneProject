3fe6374d0a8152cb3a15268c2a99984a64418392
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2023-04-24 14:00:23 -0600

    shrink headings and add username

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -9,14 +9,15 @@ const Home = () => {
         <div>
             <Header />
             <div className='content-wrap'>
-                <h1>Dashboard</h1>
+                <h2>Dashboard</h2>
                 <a href="/play"><button className='large-button'>Play Game</button></a>
                 <br/>
                 <a href={"/recordings/" + sessionStorage.getItem("user")}><button>Game History</button></a>
                 <a href="/account"><button>Your Account</button></a>
                 <div className='flex-container'>
                     <div className='flex-child home-left-section'>
-                        <h3>Your Stats:</h3>
+                        {/* eslint-disable-next-line react/no-unescaped-entities */}
+                        <h4>{sessionStorage.getItem("user")}'s Stats:</h4>
                     </div>
                     <div className='flex-child home-right-section'>
                         <p>MMR: {sessionStorage.getItem("mmr")}</p>
